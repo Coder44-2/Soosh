a078045767f6433f8a8e90431a3f75d8d025cf81
Mahir Numayeer Islam <mahir.numayeer@gmail.com>
2020-05-28 15:52:34 +0800

    Finished the tutorial

--- app/src/main/java/com/example/soosh/MainActivity.kt
+++ app/src/main/java/com/example/soosh/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.soosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getStartedBtn.setOnClickListener {
+            val leagueIntnt = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntnt)
+        }
     }
 }
